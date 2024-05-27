5e71c8dc14ada472bdbceae3b02ee225f3d95b04
Ruben Chobanyan <ru.chobanyan@icloud.com>
2024-05-26 19:01:29 -0700

    fixed bugs functions

--- js/functions.js
+++ js/functions.js
@@ -1,41 +1,39 @@
-//функция проверки является сливо палидроном
-let str = 'abcdedcba';
-str = str.toLowerCase();
-function isPalindrome(text) {
-  const strLength = text.length;
-  for (let i = 0; i < strLength; i++) {
-    if (text[0] === text[strLength - 1 - i]) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  return true;
-}
-isPalindrome(str);
-
-//извлечение чисел от 0 до 9 из передаваемой строки
-
-const strNumber = 'ECMAScript 2022';
-function isNum(value) {
-  const strLengthNum = value.length;
-  let res = '';
-  for (let i = 0; i < strLengthNum; i++) {
-    if (!isNaN(value[i])) {
-      res += value[i];
-    }
+//
+const myPadStart = (string, minLength, addSymbol) => {
+  let res = string;
+  const tmpResLength = res.length + addSymbol.length;
+  let actPad = '';
+  while (res.length < minLength) {
+    actPad = tmpResLength <= minLength ? addSymbol : addSymbol.slice(0, minLength - tmpResLength);
+    res = actPad + res;
   }
   return res;
-}
-isNum(strNumber);
+};
+myPadStart('1', 2, '0');
+
+//функция проверки является слово палидроном
+const isPalindrome = (text) => {
+  const tempText = text.toLowerCase().replaceAll(' ', '');
+  let reverseText = '';
+  for (let i = tempText.length - 1; i >= 0; i--) {
+    reverseText += tempText.at(i);
+  }
+  return tempText === reverseText;
+};
+isPalindrome('');
+
+//извлечение чисел от 0 до 9 из передаваемой строки
+const getNumber = (value) => {
+  let res = '';
+  for (let i = 0; i < value.length; i++) {
+    if (!Number.isNaN(parseInt(value.at(i), 10))) {
+      res += value.at(i);
+    }
+  }
+  return parseInt(res, 10);
+};
+getNumber('f32sda222');
 
 //проверка длины строки
-
-function chekStrLength(string, maxLength) {
-  if (string.length <= maxLength) {
-    return true;
-  } else {
-    return false;
-  }
-}
-chekStrLength('qwertyui',6);
+const chekStrLength = (string, maxLength) => string.length <= maxLength;
+chekStrLength('dsad', 4);
